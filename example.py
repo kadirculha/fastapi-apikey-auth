87a28c3db6111edbd84ed49cae1258d9343a383e
Abdulkadir <84346396+kadirculha@users.noreply.github.com>
2024-08-16 16:55:35 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,44 +1,53 @@
-from fastapi import FastAPI, Body, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-
-api_keys = [
-    "akljnv13bvi2vfo0b0bw"
-]  # This is encrypted in the database
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")  # use token authentication
+from fastapi import HTTPException, status, Security, FastAPI
+from fastapi.security import APIKeyHeader, APIKeyQuery
 
 
-def api_key_auth(api_key: str = Depends(oauth2_scheme)):
-    if api_key not in api_keys:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Forbidden"
-        )
+API_KEYS = [
+    "9d207bf0-10f5-4d8f-a479-22ff5aeff8d1",
+    "f47d4a2c-24cf-4745-937e-620a5963c0b8",
+    "b7061546-75e8-444b-a2c4-f19655d07eb8",
+]
 
+api_key_query = APIKeyQuery(name="api-key", auto_error=False)
+api_key_header = APIKeyHeader(name="x-api-key", auto_error=False)
+
+def get_api_key(api_key_query: str = Security(api_key_query), api_key_header: str = Security(api_key_header)) -> str:
+    """Retrieve and validate an API key from the query parameters or HTTP header.
+
+    Args:
+        api_key_query: The API key passed as a query parameter.
+        api_key_header: The API key passed in the HTTP header.
+
+    Returns:
+        The validated API key.
+
+    Raises:
+        HTTPException: If the API key is invalid or missing.
+    """
+    if api_key_query in API_KEYS:
+        return api_key_query
+    if api_key_header in API_KEYS:
+        return api_key_header
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid or missing API Key",
+    )
+    
 
 app = FastAPI()
 
+@app.get("/public")
+def public():
+    """A public endpoint that does not require any authentication."""
+    return "Public Endpoint"
 
-@app.get("/protected", dependencies=[Depends(api_key_auth)])
-def add_post() -> dict:
-    return {
-        "data": "You used a valid API key."
-    }
+@app.get("/private")
+def private(api_key: str = Security(get_api_key)):
+    """A private endpoint that requires a valid API key to be provided."""
+    return f"Private Endpoint. API Key: {api_key}"
 
 
-####################################
 
-
-# call API
-import requests
-
-url = "http://localhost:8000/protected"
-
-# The client should pass the API key in the headers
-headers = {
-  'Content-Type': 'application/json',
-  'Authorization': 'Bearer akljnv13bvi2vfo0b0bw'
-}
-
-response = requests.get(url, headers=headers)
-print(response.text)  # => "You used a valid API key."
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("api_key:app", host="0.0.0.0", port=8090, reload=True)
